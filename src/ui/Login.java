4f67070025b5c169ac34950548b6335b41217375
Mohamed <mohanader2244@gmail.com>
2018-03-30 15:08:55 +0200

    add comment to button login

--- src/ui/Login.java
+++ src/ui/Login.java
@@ -171,6 +171,7 @@ public class Login extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        // login button
         try
         {
             String driver = "com.mysql.jdbc.Driver";
